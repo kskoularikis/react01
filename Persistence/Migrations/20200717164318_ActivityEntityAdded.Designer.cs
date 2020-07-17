c93afc4689bac6401fed90256cd2b69937241b17
kskoularikis <kskoularikis@gmail.com>
2020-07-17 23:24:43 +0100

    Base Api

--- Persistence/Migrations/20200717164318_ActivityEntityAdded.Designer.cs
+++ Persistence/Migrations/20200717164318_ActivityEntityAdded.Designer.cs
@@ -2,15 +2,15 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using Persistence;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Persistence.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200717164318_ActivityEntityAdded")]
+    partial class ActivityEntityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
